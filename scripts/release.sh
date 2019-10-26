2f269abb1d86bedb685c8eda0b8ac08dbcca85d1
Luke Hansford <mail@lukehansford.me>
2019-10-26 13:39:59 +0200

    Add feedly as app

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,3 @@
-SCRIPT="cd dashboard; git pull; yarn && yarn build;"
+SCRIPT="cd dashboard; git pull; sudo rm -r build; yarn && yarn build;"
 # Note: blog needs to be set in `~/.ssh/config`
 ssh blog "${SCRIPT}"
