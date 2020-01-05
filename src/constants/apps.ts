c3016afb36f090f633bac23cd32ac41355999914
Luke Hansford <mail@lukehansford.me>
2020-01-05 21:04:47 +0100

    Fix YNAB link

--- src/constants/apps.ts
+++ src/constants/apps.ts
@@ -2,7 +2,7 @@ import { faMapPin, faHeadset, faDollarSign, faRssSquare, faHome } from '@fortawe
 
 export const APPS = [
   { name: 'Feedly', link: 'https://feedly.com/i/my', icon: faRssSquare},
-  { name: 'YNAB', link: 'https://app.youneedabudget.com/70ae8da7-d394-43a9-9fba-90f2107e2fcd/budget/201909', icon: faDollarSign},
+  { name: 'YNAB', link: 'https://app.youneedabudget.com/70ae8da7-d394-43a9-9fba-90f2107e2fcd/', icon: faDollarSign},
   { name: 'Monica', link: 'https://app.monicahq.com/dashboard', icon: faHeadset},
   { name: 'Pinboard', link: 'https://pinboard.in/u:lhansford/', icon: faMapPin},
   { name: 'Blog', link: 'http://lukehansford.me/', icon: faHome},
